a23bbc4697954b54dd4fb668a52053c96c2eed74
Michał Rusin <michalrusin016@gmail.com>
2015-01-18 23:30:48 +0100

    Added destroyable wall

---
+++ Assets/Scripts/StoneParticle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneParticle : MonoBehaviour {
+
+    public GameObject inStoneParticlePrefab;
+    private bool isActive = true;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "castle")
+        {
+            if (isActive)
+            {
+                GameObject newBullet = Instantiate(inStoneParticlePrefab, transform.position, transform.rotation) as GameObject;
+                isActive = false;
+
+                DestroyObject(gameObject);
+
+
+                collision.collider.GetComponent<WallDestroy>().hit();
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WallDestroy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallDestroy : MonoBehaviour {
+
+    private int destroylevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    public GameObject inBrick;
+    public GameObject inBrickSmall;
+    public Sprite inStoneCrack;
+    public Sprite inStoneCrack2;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void hit()
+    {
+        destroylevel++;
+
+        if (destroylevel == 1)
+        {
+            spriteRenderer.sprite = inStoneCrack;
+        }
+        else if (destroylevel == 2)
+        {
+            spriteRenderer.sprite = inStoneCrack2;
+        }
+        else if (destroylevel == 3)
+        {
+            Vector3 pos = transform.position;
+            pos.x -= 0.3f;
+
+            for (int i = 0; i < 20; i++)
+            {
+                pos.x += 0.01f;
+                pos.y += 0.01f; 
+                Instantiate(inBrick, pos, transform.rotation);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                pos.x += 0.01f;
+                pos.y += 0.01f; 
+                Instantiate(inBrickSmall, pos, transform.rotation);
+            }
+
+            DestroyObject(gameObject);
+        }
+    }
+}
